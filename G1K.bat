a6aa68e7629b1328a7cb5d1d97cf8ee7929144d5
Aveiga <130158889+g1ktecnol@users.noreply.github.com>
2024-11-12 13:32:16 -0400

    Update G1K.bat

--- G1K.bat
+++ G1K.bat
@@ -70,7 +70,7 @@ goto menu
 @echo #============== BAIXANDO OFFICE 2013, AGUARDE!!! =================#
 bitsadmin /transfer Office /priority foreground https://github.com/g1ktecnol/office2013/releases/download/office2013v1.0/Office2013.exe C:\office2013.exe
 cd /
-start /office2013.exe
+start office2013.exe
 @echo #============= INSTALANDO OFFICE 2013, AGUARDE!!! ================#
 pause
 mklink "C:\Users\%username%\Desktop\Word" "C:\Program Files\Microsoft Office 15\root\office15\WINWORD.EXE" 
